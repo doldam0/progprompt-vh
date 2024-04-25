f13db0cd518c1ce9f3797898fffed4f7da1cbc82
Jang Jinwoo <jinustar19@gmail.com>
2024-04-25 11:21:50 +0900

    feat: Make task set

---
+++ utils/constants.py
@@ -0,0 +1,39 @@
+TASKS_SET = [
+    "Put pencil on desk",
+    "Put cellphone on bed",
+    "Put book on sofa",
+    "Put egg on microwave",
+    "Turn on desklamp",
+    "Turn on floorlamp",
+    "Place tissuebox in toilet",
+    "Place tomato in microwave",
+    "Place winebottle in bookshelf",
+    "Open refrigerator",
+    "Open microwave",
+]
+
+SUCCESS_CONDITIONS = [
+    [("pencil", "on", "desk")],
+    [("cellphone", "on", "bed")],
+    [("book", "on", "sofa")],
+    [("egg", "on", "microwave")],
+    [("desklamp", "is", "on")],
+    [("floorlamp", "is", "on")],
+    [("tissuebox", "inside", "toilet")],
+    [("tomato", "inside", "microwave")],
+    [("winebottle", "inside", "bookshelf")],
+    [("refrigerator", "is", "open")],
+    [("microwave", "is", "open")],
+]
+
+QUERIES_SET = [
+    ("tv", "is", "off"),
+    ("radio", "is", "off"),
+    ("microwave", "is", "off"),
+    ("stove", "is", "off"),
+    ("computer", "is", "off"),
+    ("cabinet", "is", "closed"),
+    ("microwave", "is", "closed"),
+    ("stove", "is", "closed"),
+    ("dishwasher", "is", "closed"),
+]
